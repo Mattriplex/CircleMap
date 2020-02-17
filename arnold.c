068c7bb7f8bbe27bfe3708b594b1f689f8bf40dd
Matttriplex <marosini@bluewin.ch>
2020-02-17 19:07:07 +0100

    decoupled from png

--- arnold.c
+++ arnold.c
@@ -8,7 +8,7 @@
 
 #define RECINIT 0.1f
 #define RMFIRST 200
-#define ITLIMIT 300
+#define ITLIMIT 250
 #define ABS(X) ((X) >= 0.0 ? (X) : -(X))
 #define MOD1(X) ((X) - floor(X))
 #define A_RES 10.0
@@ -36,7 +36,9 @@ bitmap_t;
     
 typedef struct
 {
-    bitmap_t bitmap;
+    unsigned *data;
+    size_t rows; // Range of Omega
+    size_t cols; // Range of K
     double o_low;
     double k_low;
     double o_stepsz;
@@ -51,107 +53,11 @@ typedef struct
     size_t n;
 } args_t;
 
-/* Given "bitmap", this returns the pixel of bitmap at the point 
-   ("x", "y"). */
-
-static pixel_t * pixel_at (bitmap_t * bitmap, int x, int y)
+static unsigned * point_at (plot_t * plot, size_t r, size_t c) 
 {
-    return bitmap->pixels + bitmap->width * y + x;
-}
-    
-/* Write "bitmap" to a PNG file specified by "path"; returns 0 on
-   success, non-zero on error. */
+    return plot->data + plot->rows * c + r;
+}    
 
-static int save_png_to_file (bitmap_t *bitmap, const char *path)
-{
-    FILE * fp;
-    png_structp png_ptr = NULL;
-    png_infop info_ptr = NULL;
-    size_t x, y;
-    png_byte ** row_pointers = NULL;
-    /* "status" contains the return value of this function. At first
-       it is set to a value which means 'failure'. When the routine
-       has finished its work, it is set to a value which means
-       'success'. */
-    int status = -1;
-    /* The following number is set by trial and error only. I cannot
-       see where it it is documented in the libpng manual.
-    */
-    int pixel_size = 3;
-    int depth = 8;
-    
-    fp = fopen (path, "wb");
-    if (! fp) {
-        goto fopen_failed;
-    }
-
-    png_ptr = png_create_write_struct (PNG_LIBPNG_VER_STRING, NULL, NULL, NULL);
-    if (png_ptr == NULL) {
-        goto png_create_write_struct_failed;
-    }
-    
-    info_ptr = png_create_info_struct (png_ptr);
-    if (info_ptr == NULL) {
-        goto png_create_info_struct_failed;
-    }
-    
-    /* Set up error handling. */
-
-    if (setjmp (png_jmpbuf (png_ptr))) {
-        goto png_failure;
-    }
-    
-    /* Set image attributes. */
-
-    png_set_IHDR (png_ptr,
-                  info_ptr,
-                  bitmap->width,
-                  bitmap->height,
-                  depth,
-                  PNG_COLOR_TYPE_RGB,
-                  PNG_INTERLACE_NONE,
-                  PNG_COMPRESSION_TYPE_DEFAULT,
-                  PNG_FILTER_TYPE_DEFAULT);
-    
-    /* Initialize rows of PNG. */
-
-    row_pointers = png_malloc (png_ptr, bitmap->height * sizeof (png_byte *));
-    for (y = 0; y < bitmap->height; y++) {
-        png_byte *row = 
-            png_malloc (png_ptr, sizeof (uint8_t) * bitmap->width * pixel_size);
-        row_pointers[y] = row;
-        for (x = 0; x < bitmap->width; x++) {
-            pixel_t * pixel = pixel_at (bitmap, x, y);
-            *row++ = pixel->red;
-            *row++ = pixel->green;
-            *row++ = pixel->blue;
-        }
-    }
-    
-    /* Write the image data to "fp". */
-
-    png_init_io (png_ptr, fp);
-    png_set_rows (png_ptr, info_ptr, row_pointers);
-    png_write_png (png_ptr, info_ptr, PNG_TRANSFORM_IDENTITY, NULL);
-
-    /* The routine has successfully written the file, so we set
-       "status" to a value which indicates success. */
-
-    status = 0;
-    
-    for (y = 0; y < bitmap->height; y++) {
-        png_free (png_ptr, row_pointers[y]);
-    }
-    png_free (png_ptr, row_pointers);
-    
- png_failure:
- png_create_info_struct_failed:
-    png_destroy_write_struct (&png_ptr, &info_ptr);
- png_create_write_struct_failed:
-    fclose (fp);
- fopen_failed:
-    return status;
-}
 
 /* Given "value" and "max", the maximum value which we expect "value"
    to take, this returns an integer between 0 and 255 proportional to
@@ -210,7 +116,6 @@ pixel_t color_from_recn(int n) {
     return colo;
 }
 
-
 void * color_segment(void* args) {
     args_t *a = (args_t *) args;
     plot_t plot = *(a->plot);
@@ -220,27 +125,42 @@ void * color_segment(void* args) {
     double o = plot.o_low + offset * plot.o_stepsz;
     for (size_t oidx = offset; oidx < offset + n; oidx++, o += plot.o_stepsz) {
         double k = plot.k_low;
-        for (size_t kidx = 0; kidx < plot.bitmap.height; kidx++, k += plot.k_stepsz) {
-            int n = get_recn(o, k, plot.eps,0.5);
-            pixel_t colo = color_from_recn(n);
-            pixel_t *px = pixel_at (&plot.bitmap, oidx, kidx);
-            px->red = colo.red;
-            px->green = colo.green;
-            px->blue = colo.blue;
+        for (size_t kidx = 0; kidx < plot.rows; kidx++, k += plot.k_stepsz) {
+            unsigned n = get_recn(o, k, plot.eps, 0.5);
+            unsigned *pt = point_at (&plot, kidx, oidx);
+            *pt = n;
         }
     }
     return NULL;
 }
 
+void save_to_csv(plot_t plot, const char *path) {
+    FILE * fp = fopen (path, "w");
+    if (!fp) {
+        printf("Error: Couldn't open \"%s\"", path);
+    }
+    for (size_t r = 0; r < plot.rows; r++) {
+        size_t c;
+        for (c = 0; c < plot.cols - 1; c++) {
+            fprintf(fp,"%d,", plot.data[c * plot.rows + r]);
+        }
+        fprintf(fp, "%d\n", plot.data[c * plot.rows + r]);
+    }
+    fclose(fp);
+}
+
 void mk_plot(size_t k_steps, size_t o_steps, double k_low, double k_high, double o_low, double o_high, double eps) {
     /* Set up bitmap and calculate parameter steps*/
     plot_t plot;
-    plot.bitmap.width = o_steps;
-    plot.bitmap.height = k_steps;
-    plot.bitmap.pixels = calloc(o_steps * k_steps, sizeof(pixel_t));
+    plot.cols = o_steps;
+    plot.rows = k_steps;
+    plot.data = calloc(o_steps * k_steps, sizeof(unsigned));
+    plot.o_low = o_low;
+    plot.k_low = k_low;
     plot.k_stepsz = (k_high - k_low) / k_steps;
     plot.o_stepsz = (o_high - o_low) / o_steps;
-    if (!plot.bitmap.pixels)
+    plot.eps = eps;
+    if (!plot.data)
         exit(1);
     
     /* Parallelize Workload */
@@ -264,16 +184,13 @@ void mk_plot(size_t k_steps, size_t o_steps, double k_low, double k_high, double
 
     /* Save File */
     char fname[100];
-    snprintf(fname, 100, "arnold_%d.png", (unsigned)time(NULL));
-    if (save_png_to_file (& plot.bitmap, fname)) {
-	fprintf (stderr, "Error writing file.\n");
-    }
-    free(plot.bitmap.pixels);
+    snprintf(fname, 100, "output/arnold_%d.csv", (unsigned)time(NULL));
+    save_to_csv(plot, fname);
+    free(plot.data);
 }
 
 int main ()
 {
-    /* Write the image to a file 'fruit.png'. */
-    mk_plot(600, 600, 0.0f, 4*M_PI, -0.5f, 1.0f, 0.0001f);
+    mk_plot(3000, 1500, 0.0f, 4*M_PI, 0.0f, 1.0f, 0.0001f);
   
 }
